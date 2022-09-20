2176e3044429e37952704c4d31077b8401a8ca97
jeramee <jerameeoliver@gmail.com>
2022-09-19 21:39:13 -0500

    Add folders

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!  Jeramee Oliver';
+
+// Assign variable myNumber
+let myNumber = 5;
+
+// Assign variable myOtherNumber
+let myOtherNumber = 5;
+
+// Assign variable myTotal as the product of the two variables
+let myTotal = myNumber * myOtherNumber;
+
+// Assign variable myName to Jeramee
+let myName = Jeramee;
+
+// Assign variable myTotal as the sum of the two variables
+let myCombo = myNumber + myOtherNumber;
+
+// Assign variable myRemainder as the remainder of the myNumber/5
+let myRemainder = myNumber % 5;
+
+// Assign variable i 
+let i = 2;
+
+// Assign variable int
+let int = 4;
+
+// Assign variable myBoolean
+let myBoolean = false;
